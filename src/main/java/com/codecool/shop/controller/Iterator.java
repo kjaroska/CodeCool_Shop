446e024fbfc6d899b6326670e56ecee408cd7fe9
Marek Stopka <keramakpots@gmail.com>
2017-04-25 17:30:41 +0200

    add iterator interface

---
+++ src/main/java/com/codecool/shop/controller/Iterator.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.controller;
+
+/**
+ * Created by marek on 25.04.17.
+ */
+interface Iterator {
+    java.util.Iterator Iterator();
+
+    Boolean hasNext();
+
+    Object next();
+
+}

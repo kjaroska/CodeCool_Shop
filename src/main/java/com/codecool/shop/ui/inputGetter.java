daa35b745a7f29047ae9cbf587adea059bfe694b
Marek Stopka <keramakpots@gmail.com>
2017-04-25 14:55:32 +0200

    add class to get input from user

---
+++ src/main/java/com/codecool/shop/ui/inputGetter.java
@@ -0,0 +1,86 @@
+package com.codecool.shop.ui;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by marek on 17.04.17.
+ */
+public class inputGetter {
+
+    private static Scanner getScanner() {
+        return new Scanner(System.in);
+    }
+
+    public static String getStringInput() {
+        Scanner scanner = getScanner();
+        return scanner.next();
+    }
+
+    public static String getLineInput() {
+        Scanner scanner = getScanner();
+        return scanner.nextLine();
+    }
+
+    public static Integer getIntegerInput() {
+        Scanner scanner = getScanner();
+        boolean bError = true;
+        Integer intInput = 0;
+        while (bError) {
+            try {
+                if (scanner.hasNextInt()) {
+                    intInput = scanner.nextInt();
+                }
+            } catch (Exception e) {
+                System.out.print(e.toString());
+                scanner.reset();
+                scanner.hasNextInt();
+                continue;
+            }
+            bError = false;
+        }
+        return intInput;
+    }
+
+    public static Float getFloatInput() {
+        Scanner scanner = getScanner();
+        scanner.useLocale(Locale.US);
+        Float price = 0.0f;
+        Boolean search = true;
+        while (search) {
+            System.out.print("Enter a float price: ");
+            try {
+                price = scanner.nextFloat();
+                search = false;
+            } catch (InputMismatchException ime) {
+                System.out.println(ime.toString());
+                scanner.reset();
+                scanner.next();// Flush the buffer from all data
+            }
+        }
+        return price;
+    }
+
+    public static Date getDate() {
+        String date = inputGetter.getLineInput();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date date2 = null;
+        Boolean search = true;
+        while (search) {
+            try {
+                //Parsing the String
+                date2 = dateFormat.parse(date);
+                search = false;
+            } catch (ParseException e) {
+                System.out.println("Bad format try (DD-MM-YYYY)");
+                date = inputGetter.getLineInput();
+            }
+        }
+        return date2;
+    }
+}
+

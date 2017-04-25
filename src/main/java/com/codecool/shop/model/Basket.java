8696ea60cfbf60e11d9b3bd9cc9a726be1bdeda8
Marek Stopka <keramakpots@gmail.com>
2017-04-25 20:34:49 +0200

    add basket class

---
+++ src/main/java/com/codecool/shop/model/Basket.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.model;
+
+import com.codecool.shop.controller.ProductIterator;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by marek on 25.04.17.
+ */
+public class Basket {
+
+    final ArrayList<Product> productList;
+    private final Iterator<Product> iterator;
+
+    public Basket(ArrayList<Product> productList) {
+        this.productList = productList;
+        this.iterator = getIterator(this.productList);
+    }
+
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+    public boolean removeProduct(Product product) {
+        boolean found;
+        if (productList.contains(product)) {
+            productList.remove(product);
+            found = true;
+        } else {
+            found = false;
+        }
+        return found;
+    }
+
+    private Iterator<Product> getIterator(ArrayList<Product> productList) {
+        return new ProductIterator(productList).Iterator();
+    }
+
+    public void throughBasket() {
+        for (Iterator<Product> iter = this.getIterator(productList); iter.hasNext(); ) {
+            Product product = iter.next();
+            System.out.println("Product : " + product.getName());
+        }
+    }
+}

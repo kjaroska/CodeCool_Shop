8a4ba3a90eab7c3a69fabb8893cae792fb7d217e
Marek Stopka <keramakpots@gmail.com>
2017-04-25 17:50:43 +0200

    implement main method in main to show all products

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import com.codecool.shop.controller.ProductController;
+import com.codecool.shop.dao.ProductDaoImpl;
+import com.codecool.shop.model.Product;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Product> products = new ProductDaoImpl().getAll();
+        Iterator<Product> productIterator = ProductController.getIterator(products);
+        while (productIterator.hasNext()) {
+            Product product = productIterator.next();
+            System.out.println(product.toString());
+        }
 
     }
 

d1780665cd9604a7972c16d0102e7aa75413bc15
Marek Stopka <keramakpots@gmail.com>
2017-04-25 19:23:18 +0200

    implement method show available products in product controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,14 +7,6 @@ import java.util.Iterator;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Product> products = new ProductDaoImpl().getAll();
-        Iterator<Product> productIterator = ProductController.getIterator(products);
-        while (productIterator.hasNext()) {
-            Product product = productIterator.next();
-            System.out.println(product.toString());
-        }
-
+        ProductController.showAvailableProducts();
     }
-
-
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -4,13 +4,24 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 
 
+import com.codecool.shop.dao.ProductDaoImpl;
 import com.codecool.shop.model.Product;
 import java.util.*;
+import java.util.Iterator;
 
 public class ProductController {
 
     public static java.util.Iterator<Product> getIterator(ArrayList<Product> productList) {
         return new ProductIterator(productList).Iterator();
+    }
+
+    public static void showAvailableProducts() {
+        ArrayList<Product> products = new ProductDaoImpl().getAll();
+        Iterator<Product> productIterator = ProductController.getIterator(products);
+        while (productIterator.hasNext()) {
+            Product product = productIterator.next();
+            System.out.println(product.toString());
+        }
     }
 
 }

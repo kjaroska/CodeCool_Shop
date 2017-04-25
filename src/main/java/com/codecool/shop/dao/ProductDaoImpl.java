4078b3abd7e07e01fe9e2ab759a598a3d4cc5bcb
Marek Stopka <keramakpots@gmail.com>
2017-04-25 23:00:48 +0200

    change error message in product dao

--- src/main/java/com/codecool/shop/dao/ProductDaoImpl.java
+++ src/main/java/com/codecool/shop/dao/ProductDaoImpl.java
@@ -49,7 +49,7 @@ public class ProductDaoImpl implements ProductDao {
                 listProducts.add(newProduct);
             }
         } catch (Exception e) {
-            System.err.println(e.getClass().getName() + " in product :" + e.getMessage());
+            System.err.println(e.getClass().getName() + " in productdaoimpl :" + e.getMessage());
             System.exit(0);
         }
         return listProducts;

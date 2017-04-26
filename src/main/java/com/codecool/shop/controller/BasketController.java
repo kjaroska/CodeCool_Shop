22d10de15917184564f24b7dda34d6bfb9be4261
Marek Stopka <keramakpots@gmail.com>
2017-04-26 15:40:33 +0200

    add error handling in add to basket in basketcontroller

--- src/main/java/com/codecool/shop/controller/BasketController.java
+++ src/main/java/com/codecool/shop/controller/BasketController.java
@@ -5,7 +5,7 @@ import com.codecool.shop.model.Basket;
 import com.codecool.shop.model.Item;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.view.Printer;
-import com.codecool.shop.ui.inputGetter;
+import com.codecool.shop.ui.InputGetter;
 import java.util.Iterator;
 
 /**
@@ -20,10 +20,22 @@ public class BasketController {
 
     public static Basket addToBasket(Basket basket) {
         Printer.printObject("Which product you want to add? ");
-        Integer productId = inputGetter.getIntegerInput();
-        Product product = new ProductDaoImpl().find(productId);
-        Printer.printObject("How many " + product.getName() + " do you want? ");
-        Integer quantity = inputGetter.getIntegerInput();
+        Integer productId = InputGetter.getIntegerInput();
+        Product product = null;
+        while (true) {
+            try {
+                product = new ProductDaoImpl().find(productId);
+                Printer.printObject("How many " + product.getName() + " do you want? ");
+                if (product != null) {
+                    break;
+                }
+            } catch (Exception e) {
+                Printer.printObject(e + ", No product with given id");
+                productId = InputGetter.getIntegerInput();
+                continue;
+            }
+        }
+        Integer quantity = InputGetter.getIntegerInput();
         Item item = new Item(product, quantity);
         basket.addProduct(item);
         return basket;

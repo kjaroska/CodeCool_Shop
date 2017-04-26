8eddf2a5aaaa13f948a3dea639e9e28f940ca8e1
Krzysztof <kjaroska@gmail.com>
2017-04-26 19:44:55 +0200

    Add Basket import and two methods: printOrderSummary and printFakePayment

--- src/main/java/com/codecool/shop/view/Printer.java
+++ src/main/java/com/codecool/shop/view/Printer.java
@@ -1,8 +1,9 @@
 package com.codecool.shop.view;
 
+import com.codecool.shop.model.Basket;
 import com.codecool.shop.model.Item;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 
 public class Printer {
     public static void printObject(String stringObject) {
@@ -14,6 +15,26 @@ public class Printer {
             System.out.println(product.toString());
         }
     }
+
+  public static void printOrderSummary(Basket basket) {
+    System.out.println("Your order summary:");
+    Float orderPrice = 0.0f;
+    for (Item product : basket.getItemList()) {
+      System.out.println(product);
+      orderPrice = orderPrice + product.getTotalPrice();
+    }
+    System.out.println("\nOrder overall price: " + orderPrice);
+    System.out.println("Advance to payment?");
+  }
+
+  public static void printFakePayment() {
+    ArrayList<String> paymentOptions = new ArrayList<>(Arrays.asList("Choose option",
+        "[1] PAY IN CASH", "[2] PAY BY CARD", "[3] OTHER",
+        "[0] EXIT."));
+    for (String option : paymentOptions) {
+      System.out.println(option);
+    }
+  }
 
     public static void printEmpty() {
         System.out.println("");

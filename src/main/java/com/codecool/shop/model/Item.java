0b10d843303596b283aa68bd317915cc60b7217e
Marek Stopka <keramakpots@gmail.com>
2017-04-26 13:28:16 +0200

    Add item class

--- src/main/java/com/codecool/shop/model/Item.java
+++ src/main/java/com/codecool/shop/model/Item.java
@@ -5,4 +5,27 @@ package com.codecool.shop.model;
  */
 public class Item {
 
+    private Product product;
+
+
+    private Integer quantity;
+    private Float totalPrice;
+
+    public Item(Product product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = product.getDefaultPrice() * quantity;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public Float getTotalPrice() {
+        return this.totalPrice;
+    }
 }

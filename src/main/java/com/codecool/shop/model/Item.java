31f8d4cfb7f3329aa0c0fdd1eb5d431a90a9cf3c
Marek Stopka <keramakpots@gmail.com>
2017-04-26 19:53:10 +0200

    Change item to string method

--- src/main/java/com/codecool/shop/model/Item.java
+++ src/main/java/com/codecool/shop/model/Item.java
@@ -44,7 +44,7 @@ public class Item {
     public String toString() {
         return "Item: " +
             "id=" + id +
-            "product=" + product.getName() +
+            " product=" + product.getName() +
             ", quantity=" + quantity +
             ", totalPrice=" + totalPrice;
     }

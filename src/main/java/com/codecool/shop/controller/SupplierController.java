892d4fc3344f1b593b230c26def91109df83eab2
zBIGniew <zbigniew.piotr.tokarski@gmail.com>
2017-04-26 14:16:00 +0200

    modify SupplierController class in order to complete productBySupplier function

--- src/main/java/com/codecool/shop/controller/SupplierController.java
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -1,19 +1,32 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.SupplierDaoImpl;
+import com.codecool.shop.dao.ProductDaoImpl;
 import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.view.Printer;
 import java.util.ArrayList;
+import java.util.Iterator;
+import com.codecool.shop.ui.inputGetter;
 
-/**
- * Created by marek on 25.04.17.
- */
 public class SupplierController {
 
     public static void showAvailableSuppliers() {
         ArrayList<Supplier> suppliers = new SupplierDaoImpl().getAll();
         for (Supplier supplier : suppliers) {
             Printer.printObject(supplier.toString());
+        }
+    }
+    public static void productBySuppliers() {
+        Printer.printObject("\nEnter Supplier's Id");
+        Integer supplierId = inputGetter.getIntegerInput();
+        Supplier supplier = new SupplierDaoImpl().find(supplierId);
+        ArrayList<Product> productsFromSupplier = new ProductDaoImpl().getBy(supplier);
+        Iterator<Product> iterProducts = productsFromSupplier.iterator();
+        Printer.printObject("\n'"+ supplier.getName() +"' products:\n");
+        while (iterProducts.hasNext()) {
+            Product singleProduct = iterProducts.next();
+            Printer.printObject(singleProduct.toString());
         }
     }
 

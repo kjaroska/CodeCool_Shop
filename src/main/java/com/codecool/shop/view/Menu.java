720f9a737f327e28fd683d5dd48c7638f6a9efd5
Archi91 <olshovskiy@gmail.com>
2017-04-26 13:39:34 +0200

    Add ArrayList mainMenu in Menu class

--- src/main/java/com/codecool/shop/view/Menu.java
+++ src/main/java/com/codecool/shop/view/Menu.java
@@ -1,6 +1,18 @@
 package com.codecool.shop.view;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Menu {
 
+  private static final ArrayList<String> mainMenu = new ArrayList<>(Arrays.asList("Choose option",
+      "[1] SHOW PRODUCTS BY PRODUCT CATEGORY.", "[2] SHOW PRODUCTS BY SUPPLIER.",
+      "[3] SHOW ALL AVAILABLE PRODUCTS.",
+      "[4] SHOW MY BASKET.",
+      "[0] EXIT."));
+
+  public static ArrayList<String> getMainMenu() {
+    return mainMenu;
+  }
 }

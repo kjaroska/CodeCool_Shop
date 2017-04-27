ad1ade177b86f7a8199eff3db0c74afc76fc5a2c
Marek Stopka <keramakpots@gmail.com>
2017-04-27 11:47:28 +0200

    Add basket id as a basket attribute

--- src/main/java/com/codecool/shop/model/Basket.java
+++ src/main/java/com/codecool/shop/model/Basket.java
@@ -7,13 +7,43 @@ import java.util.Iterator;
 public class Basket {
 
     private final ArrayList<Item> itemsList;
+    private static Integer nextId = 1;
+    private final Integer id;
 
-    public ArrayList<Item> getItemList() {
-        return this.itemsList;
-    }
+    private Float totalPrice;
 
     public Basket(ArrayList<Item> items) {
         this.itemsList = items;
+        this.id = Basket.nextId++;
+        this.totalPrice = setTotalPrice();
+    }
+
+    public Basket(ArrayList<Item> items, Integer id) {
+        this.itemsList = items;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Float getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    public Float setTotalPrice() {
+        Float orderPrice = 0.0f;
+        Iterator<Item> iterItems = this.getIterator();
+        while (iterItems.hasNext()) {
+            Item item = iterItems.next();
+            orderPrice = orderPrice + item.getTotalPrice();
+        }
+
+        return this.totalPrice = orderPrice;
+    }
+
+    public ArrayList<Item> getItemList() {
+        return this.itemsList;
     }
 
     public void addProduct(Item item) {

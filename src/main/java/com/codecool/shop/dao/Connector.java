3864bb9bb8697ab1af60ef8fbb1f71a2479d38b3
Krzysztof <kjaroska@gmail.com>
2017-04-27 13:58:16 +0200

    Add getConnection, getStatement and closeConnection methods in Connector.

--- src/main/java/com/codecool/shop/dao/Connector.java
+++ src/main/java/com/codecool/shop/dao/Connector.java
@@ -4,12 +4,25 @@ import java.sql.*;
 
 public class Connector {
 
-    public static Statement connector() {
-        Connection connection;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:shop.db");
+  private static Connection connection = null;
+
+  private static Connection getConnection() {
+
+    if (connection == null) {
+      try {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:shop.db");
+      } catch (Exception e) {
+        System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        System.exit(0);
+      }
+    }
+    return connection;
+  }
+
+  private static Statement getStatement(Connection connection) {
+    Statement stmt = null;
+    try {
             stmt = connection.createStatement();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -18,8 +31,20 @@ public class Connector {
         return stmt;
     }
 
-    public static ResultSet getQueryResult(String query) {
-        Statement stmt = Connector.connector();
+  public static void closeConnection() {
+    try {
+      if (connection != null) {
+        connection.close();
+        connection = null;
+      }
+    } catch (Exception e) {
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+    }
+  }
+
+  static ResultSet getQueryResult(String query) {
+    Connection connection = getConnection();
+    Statement stmt = getStatement(connection);
         ResultSet rs = null;
         try {
             rs = stmt.executeQuery(query);

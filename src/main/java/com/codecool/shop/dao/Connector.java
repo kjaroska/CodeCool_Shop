bf3a7f2eacc42a1f47d438d861713e47adae2464
Marek Stopka <keramakpots@gmail.com>
2017-04-27 13:52:28 +0200

    Implements add basket to DB

--- src/main/java/com/codecool/shop/dao/Connector.java
+++ src/main/java/com/codecool/shop/dao/Connector.java
@@ -1,10 +1,12 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.model.Basket;
+import com.codecool.shop.model.Item;
 import java.sql.*;
 
-public class Connector {
+class Connector {
 
-    public static Statement connector() {
+    static Statement connector() {
         Connection connection;
         Statement stmt = null;
         try {
@@ -18,7 +20,7 @@ public class Connector {
         return stmt;
     }
 
-    public static ResultSet getQueryResult(String query) {
+    static ResultSet getQueryResult(String query) {
         Statement stmt = Connector.connector();
         ResultSet rs = null;
         try {
@@ -28,5 +30,32 @@ public class Connector {
             System.exit(0);
         }
         return rs;
+    }
+
+    static void addBasketToDb(Basket basket) {
+        Statement stmt = Connector.connector();
+        String sql = "INSERT INTO Basket (Id, TotalPrice) "
+            + "VALUES (" + basket.getId() + "," + basket.getTotalPrice() + ");";
+        //I want this to write the strings passed in by the method, and to the table passed in by the method
+        try {
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + " in addBasketToDb: " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    static void addItemToDb(Item item) {
+        Statement stmt = Connector.connector();
+        String sql = "INSERT INTO Items(Id,BasketId,IdProduct,Quantity) "
+            + "VALUES (" + item.getId() + "," + item.getIdBasket() + "," + item.getProduct().getId()
+            + "," + item.getQuantity() + ");";
+        //I want this to write the strings passed in by the method, and to the table passed in by the method
+        try {
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + " in addItemToDB: " + e.getMessage());
+            System.exit(0);
+        }
     }
 }

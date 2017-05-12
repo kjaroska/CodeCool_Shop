36470e6398608e54cb309c66435a8a7d61f66c26
Archi91 <aligator91>
2017-05-12 03:20:10 +0200

    Delete DbCreator class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,31 @@
 import com.codecool.shop.Application;
-import com.codecool.shop.dao.DbCreator;
+import com.codecool.shop.DatabaseManager;
+
 
 /**
  * 'O for a muse of fire!'
  **/
 class Main {
     public static void main(String[] args) {
-        DbCreator.checkArguments(args);
-        try {
-            Application.run();
-        } finally {
-            final Thread mainThread = Thread.currentThread();
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                System.out.println("Closing database connection...");
-                Application.getApp().disconnectDb();
-                System.out.println("Connection successfully closed.");
-                try {
-                    mainThread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }));
+        if (args.length != 0) {
+            DatabaseManager.run(args);
+        } else {
+            try {
+                Application.run();
+            } finally {
+                Thread mainThread = Thread.currentThread();
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    System.out.println("Closing database connection...");
+                    Application.getApp().disconnectDb();
+                    System.out.println("Connection successfully closed.");
+                    try {
+                        mainThread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }));
+            }
         }
     }
 }
+

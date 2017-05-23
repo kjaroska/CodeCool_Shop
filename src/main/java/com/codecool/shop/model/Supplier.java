29c9376c520f759fe1309258372e6f55f989d420
Grześ Bury <bury221@gmail.com>
2017-05-23 15:14:32 +0200

    remove unused toString() method

--- src/main/java/com/codecool/shop/model/Supplier.java
+++ src/main/java/com/codecool/shop/model/Supplier.java
@@ -28,15 +28,5 @@ public class Supplier extends BaseModel {
     public void addProduct(Product product) {
         this.products.add(product);
     }
-
-
-    public String toString() {
-        return String.format("id: %1$d, " +
-                        "name: %2$s, " +
-                        "description: %3$s",
-                this.id,
-                this.name,
-                this.description
-        );
     }
 }

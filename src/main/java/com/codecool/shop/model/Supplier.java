e2d31afa19530ab0a2641318ff75f31f75a4aa84
Grześ Bury <bury221@gmail.com>
2017-05-23 15:18:48 +0200

    remove products ArrayList from Supplier model

--- src/main/java/com/codecool/shop/model/Supplier.java
+++ src/main/java/com/codecool/shop/model/Supplier.java
@@ -1,20 +1,12 @@
 package com.codecool.shop.model;
 
-import java.util.ArrayList;
-
-
 public class Supplier extends BaseModel {
-
-    private ArrayList<Product> products;
 
     public Supplier(Integer id, String name, String description) {
         super(id, name, description);
-        this.products = new ArrayList<>();
     }
 
     public Supplier(String name, String description) {
         super(name, description);
-        this.products = new ArrayList<>();
-    }
     }
 }

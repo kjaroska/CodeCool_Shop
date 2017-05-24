3f6a105cfeb0a83816c15d303699a30c8ceefb2e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-05-24 15:25:52 +0200

    Delete unsued setters and getter getPrice() from Product.java

--- src/main/java/com/codecool/shop/model/Product.java
+++ src/main/java/com/codecool/shop/model/Product.java
@@ -3,7 +3,6 @@ package com.codecool.shop.model;
 import java.util.Currency;
 
 public class Product extends BaseModel {
-
     private Float defaultPrice;
     private Currency defaultCurrency;
     private ProductCategory productCategory;
@@ -29,24 +28,12 @@ public class Product extends BaseModel {
         return defaultPrice;
     }
 
-    public void setDefaultPrice(float defaultPrice) {
-        this.defaultPrice = defaultPrice;
-    }
-
     public Currency getDefaultCurrency() {
         return defaultCurrency;
     }
 
-    public void setDefaultCurrency(Currency defaultCurrency) {
-        this.defaultCurrency = defaultCurrency;
-    }
-
     public Float getPriceValue() {
         return this.defaultPrice;
-    }
-
-    public String getPrice() {
-        return String.valueOf(this.defaultPrice) + " " + this.defaultCurrency.toString();
     }
 
     private void setPrice(float price, String currency) {

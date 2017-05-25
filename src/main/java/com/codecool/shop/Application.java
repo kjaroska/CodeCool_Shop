80e742c9665392bf12ad66684aa3e5df613f4c42
Grześ Bury <bury221@gmail.com>
2017-05-25 14:13:26 +0200

    organise imports

--- src/main/java/com/codecool/shop/Application.java
+++ src/main/java/com/codecool/shop/Application.java
@@ -12,10 +12,14 @@ import com.codecool.shop.controller.ProductCategoryController;
 import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.controller.RenderingController;
 import com.codecool.shop.controller.SupplierController;
-import java.io.File;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import spark.Request;
 import spark.Response;
 import spark.Route;
